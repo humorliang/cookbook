a8976bfbe8e704081deba6e07f94637da07f02fe
humorliang <1174749673@qq.com>
2018-05-03 22:57:58 +0800

    part01 base2 over

--- base/part01/基础知识.py
+++ base/part01/基础知识.py
@@ -45,7 +45,7 @@ print('姓名%s,年龄%d' % (name, age))
 
 # a=10
 # info = input()  # 只接受接受表达式 字符串要使用‘’不然会当成变量复制给左边
-# print(info) # 把表达式的结果赋值给左边 
+# print(info) # 把表达式的结果赋值给左边
 
 
 # 用算符
@@ -53,7 +53,7 @@ print('姓名%s,年龄%d' % (name, age))
 # + 加 - 减  *乘  /除 //取整除 %取余 **幂
 
 # 赋值
-# = 
+# =
 # 复合赋值运算
 # += 加法赋值  c+=a c=c+a  -= *= /= %= **= //=
 
@@ -61,3 +61,17 @@ print('姓名%s,年龄%d' % (name, age))
 # print(help(int))
 # int(x[,base]) 将x转换为整型
 # long(x[,base]) 将x转换为长整形
+# float(x) 将x转换为一个浮点数
+# complex(real[,imag]) 创建一个复数
+# str(x) 将对象x转换为字符串
+# repr(x) 将对象x转为表达式字符串
+# eval(str) 计算str中有效的表达式并返回一个对象
+# tuple(s) 将序列s转化为一个元组
+# list(s) 将序列s转化为一个列表
+# chr(x) 将整数转换为一个字符
+# unich(x) 将整数转换为Unicode字符
+# ord(x) 将一个字符转换为他的整数值
+# hex(x) 将一个整数转换为它的十六进制字符串
+# oct(x) 将一个整数转换为一个八进制字符串
+
+
---
+++ base/part01/基础知识2.py
@@ -0,0 +1,23 @@
+# -*- coding:utf-8 -*-
+# 在python语言中用严格的缩进进行代码块控制，一般为4个空格
+# 判断语句
+a = 10
+print('----判断语句开始-----')
+if a > 0:
+    print('a是整数')
+print('----判断语句结束-----')
+
+# 比较关系运算符
+
+# == 检查两个值是否相等 返回 true 或 false
+# ！= 检查两个值是否不相等 返回 true 或 false
+# <> 检查两个值是否不相等 返回 true 或 false
+# > 大于
+# < 小于
+# >= 大于等于
+# <= 小于等于
+
+# 逻辑运算符
+# and  x and y  布尔与 x为false 返回 false 否则返回 y的计算值
+# or  x or y  布尔或 x为Ture 返回 True 否则返回 y的计算值
+# not  not x  布尔非 x为false 返回 true 否则返回 false
